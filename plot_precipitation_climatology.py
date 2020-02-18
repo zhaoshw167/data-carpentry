153a3ac1e5d12edaa7539308aa2d1e4df4a5b4ae
zhaoshw167 <zhaoshwm@163.com>
2020-02-18 11:29:08 +0800

    Ordered imports according to PEP 8

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -1,10 +1,10 @@
 import argparse
-import xarray as xr
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import numpy as np
-import cmocean
 
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+import cartopy.crs as ccrs
+import cmocean
 
 def convert_pr_units(darray):
     """Convert kg m-2 s-1 to mm day-1.
